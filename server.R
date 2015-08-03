e78fa11a88b84bcd7c4797ff5765a3b8f25a46c5
jrpepper <jrpepper@gmail.com>
2015-08-03 16:48:27 -0700

    other tweaks to Burundi map
    - adding mapbox.light background map (line 16)
    - removing if/then statements
    - updating polygon fill color to reflect
    collines@data[[input$conditions]]
    - adding popup which displays popupContent

--- server.R
+++ server.R
@@ -13,17 +13,23 @@ shinyServer(
   function(input, output, session) {
     
     output$map <- renderLeaflet({
-      leaflet(collines) %>%
+      leaflet(collines) %>% addTiles(urlTemplate = "https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWxleGdvb2RlbGwiLCJhIjoicy1fNU4wZyJ9.PjqkYLHj6u5PLiWH61TQDw") %>%
         addPolygons(stroke = FALSE, fillOpacity = 0.5, smoothFactor = 0.5,
                     color= ~colorBin("YlOrRd", collines$NUM_PATIENTS, bins=100)(NUM_PATIENTS)
         )
-      })
-
-    reactive
+    })
     
     observe({
       
+      popupContent <- paste("<strong>",collines@data[,"COLLINE"],"</strong><br>", input$conditions,": ",collines@data[[input$conditions]],"<br>", sep="")
+      
+      leafletProxy("map", session, data = collines) %>%
         clearShapes() %>%
         addPolygons(stroke = FALSE, fillOpacity = 0.5, smoothFactor = 0.5,
+                    color= ~colorBin("Blues", collines[[input$conditions]], bins=30)(collines[[input$conditions]]),
+                    popup = ~popupContent
+        )
     })
     
+  }
+)
